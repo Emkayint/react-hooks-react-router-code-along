77441235c200f2e712e7859e8adfd4af8a67d462
Martin <mkmartinoes@gmail.com>
2022-05-10 10:24:10 +0300

    Routes path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+
 function Home() {
   return (
     <div>
@@ -9,11 +10,46 @@ function Home() {
   );
 }
 
+function About(){
+  return (
+    <div>
+      <h1>This is my about components</h1>
+    </div>
+  )
+}
+
+function Login(){
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type='text' name = 'username' placeholder = 'username' />
+        </div>
+        <div>
+          <input type = "password" name = "password" placeholder="password"/>
+        </div>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+// Pages defination
+
+
 function App() {
   return (
-    <Route path='/'>
-      <Home />
-    </Route>
+    <>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/about'>
+        <About/>
+      </Route >
+      <Route exact path='/login'>
+        <Login />
+      </Route>
+  </>
   );
 }
 
