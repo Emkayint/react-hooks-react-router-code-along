9829ad8859a635573bf8b09698a964b9dfafd945
Martin <mkmartinoes@gmail.com>
2022-05-10 11:11:19 +0300

    Created Login component

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+function Login(){
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type='text' name = 'username' placeholder = 'username' />
+        </div>
+        <div>
+          <input type = "password" name = "password" placeholder="password"/>
+        </div>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -18,22 +18,7 @@ function About(){
   )
 }
 
-function Login(){
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type='text' name = 'username' placeholder = 'username' />
-        </div>
-        <div>
-          <input type = "password" name = "password" placeholder="password"/>
-        </div>
-        <input type="submit" value="submit" />
-      </form>
-    </div>
-  )
-}
+
 // Pages defination
 
 
